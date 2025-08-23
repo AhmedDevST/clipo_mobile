bb7ee6a4ce1af0b67f3cf4eb3529c4ec079e4ae8
AhmedDevST <ahmedaasli07@gmail.com>
2025-08-23 14:38:54 +0200

    configuration for the deploy in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:clipo_app/ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
